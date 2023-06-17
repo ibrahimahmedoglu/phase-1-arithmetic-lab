8bca766efc25c1a822e9b60dc685fd78ed523939
ibrahimahmedoglu <ibrahim.ahmedoglu@bahcesehir.edu.tr>
2023-06-17 14:43:22 +0300

    Completed Assigntment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4;
+
+const numbers = [1, 7, 14, 20, 3];
+const max = Math.max(...numbers);
+
+module.exports = {
+  num1,
+  num2,
+  multiply,
+  random,
+  num3,
+  num4,
+  mod,
+  max
+};
